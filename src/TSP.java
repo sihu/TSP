8110de15e16791dd725a2d92506e016e9154076f
Ludvig Jonsson <jonsson.ludde@gmail.com>
2012-11-15 16:28:31 +0100

    Naive solution

--- src/TSP.java
+++ src/TSP.java
@@ -7,6 +7,7 @@ public class TSP {
 	
 	private ArrayList<Vertex> vertices;
 	BufferedReader in;
+	int[] path;
 	
 	public static void main(String[] args) {
 		new TSP();
@@ -16,17 +17,45 @@ public class TSP {
 		vertices = new ArrayList<Vertex>();
 		in = new BufferedReader(new InputStreamReader(System.in));
 		readInstance();
+		path = findPath();
+		printSolution();
+	}
+
+	private void printSolution() {
+		for (int i = 0; i < path.length; i++) {
+			System.out.println(path[i]);
+		}
+	}
+
+	private int[] findPath() {
+		int m = vertices.size();
+		int[] path = new int[m]; 
+		boolean[] used = new boolean[m];
+		
+		for (int i = 1; i <= m; i++) {
+			int best = -1;
+			
+			for (int j = 0; j < m; j++) {
+				if (!used[j] && (best == -1 || vertices.get(path[i-1]).distanceTo(vertices.get(j)) 
+						< vertices.get(path[i-1]).distanceTo(vertices.get(best))))
+						best = j;
+			}
+			path[i-1] = best;
+			used[best] = true;
+		}
+		return path;
 	}
 
 	private void readInstance() {
 		try {
 			int m = Integer.valueOf(in.readLine());
-			for(int i = 0; i < m; i++) {
+			
+			for (int i = 0; i < m; i++) {
 				String[] vertex = in.readLine().split(" ");
 				float xCoord = Float.valueOf(vertex[0]);
 				float yCoord = Float.valueOf(vertex[1]);			
 				vertices.add(new Vertex(xCoord,yCoord));
-				System.out.println("Added coord (" + xCoord + ", " + yCoord + ")");
+//				System.out.println("Added coord (" + xCoord + ", " + yCoord + ")");
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
