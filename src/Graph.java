55c10dda35c638029297527118258f2976f861a8
jacobhak <jacobhakansson@gmail.com>
2012-11-27 11:14:22 +0100

    added buildSortedTreeFromDistanceMatrix to graph

--- src/Graph.java
+++ src/Graph.java
@@ -1,12 +1,17 @@
+import java.util.ArrayList;
+import java.util.TreeMap;
+
 
 public class Graph {
 
 	public Vertex[] vertices;
 	private float[][] distanceMatrix;
+	private ArrayList<TreeMap<Float, Integer>> idsToIdsSortedByDistance;
 	
 	public Graph(Vertex[] vertices, float[][] distanceMatrix) {
 		this.vertices = vertices;
 		this.distanceMatrix = distanceMatrix;
+		idsToIdsSortedByDistance = new ArrayList<TreeMap<Float, Integer>>();
 	}
 	
 	public float distanceBetween(int v1, int v2) {
@@ -15,6 +20,16 @@ public class Graph {
 	
 	public Vertex getVertex(int id) {
 		return vertices[id];
+	}
+	
+	public void buildSortedTreeFromDistanceMatrix() {
+		for (int i = 0; i < distanceMatrix.length; i++) {
+			idsToIdsSortedByDistance.add(i,new TreeMap<Float, Integer>());
+			for (int j = 0; j < distanceMatrix[i].length; j++) {
+				idsToIdsSortedByDistance.get(i).put(distanceMatrix[i][j],j);
+			}
+		}
+		
 	}
 	
 	public float getWeight(Path path) {
